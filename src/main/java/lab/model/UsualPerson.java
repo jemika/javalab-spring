ac2c4d2c6bbdb026d0d20e01f99e51ff4ac62f09
Vyacheslav Lapin <vyachesl@vlapin.ru>
2018-02-02 19:47:27 +0300

    lab - 05 - start
    (cherry picked from commit 7274fd0)

--- src/main/java/lab/model/UsualPerson.java
+++ src/main/java/lab/model/UsualPerson.java
@@ -1,18 +1,20 @@
 package lab.model;
 
 import lombok.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Value
+@Component("person")
 public class UsualPerson implements Person {
     private int id;
     private String firstName;
     private String lastName;
     private Country country;
     private int age;
-    private List<Contact> contacts;
     private float height;
-    private boolean programmer;
-
+    private boolean isProgrammer;
+    private boolean isBroke;
+    private List contacts;
 }
